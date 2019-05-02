a8f9d3cd5fa97f235db462c8742cf74d49f6293a
msesno <msesno@gmail.com>
2019-05-02 18:15:04 -0400

    adjust image sizes

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,27 +1,27 @@
 var friends = [
     {
     "name": "Amir",
-    "photo": "https://github.com/msesno/friendfinder/blob/master/app/data/images/amir.jpg",
+    "photo": "https://raw.githubusercontent.com/msesno/friendfinder/master/app/data/images/amir.jpg",
     "scores": ["5", "1", "4", "4", "5", "1", "2", "5", "4", "1"]
   }, {
     "name": "Bill",
-    "photo": "https://github.com/msesno/friendfinder/blob/master/app/data/images/billgates.jpg",
+    "photo": "https://raw.githubusercontent.com/msesno/friendfinder/master/app/data/images/billgates.jpg",
     "scores": ["4", "2", "5", "1", "3", "2", "2", "1", "3", "2"]
   }, {
     "name": "Jenny",
-    "photo": "https://github.com/msesno/friendfinder/blob/master/app/data/images/jenny.jpeg",
+    "photo": "https://raw.githubusercontent.com/msesno/friendfinder/master/app/data/images/jenny.jpeg",
     "scores": ["5", "2", "2", "2", "4", "1", "3", "2", "5", "5"]
   }, {
     "name": "Felicia",
-    "photo": "https://github.com/msesno/friendfinder/blob/master/app/data/images/felicia.jpeg",
+    "photo": "https://raw.githubusercontent.com/msesno/friendfinder/master/app/data/images/felicia.jpeg",
     "scores": ["3", "3", "4", "2", "2", "1", "3", "2", "2", "3"]
   }, {
     "name": "Donald",
-    "photo": "https://github.com/msesno/friendfinder/blob/master/app/data/images/donald.jpg",
+    "photo": "https://raw.githubusercontent.com/msesno/friendfinder/master/app/data/images/donald.jpg",
     "scores": ["4", "3", "4", "1", "5", "2", "5", "3", "1", "4"]
   }, {
     "name": "Simmons",
-    "photo": "https://github.com/msesno/friendfinder/blob/master/app/data/images/simmons.jpeg",
+    "photo": "https://raw.githubusercontent.com/msesno/friendfinder/master/app/data/images/simmons.jpg",
     "scores": ["4", "4", "2", "3", "2", "2", "3", "2", "4", "5"]
   }];
   
