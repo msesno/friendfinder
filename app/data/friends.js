d5593a72e9f649d8d57ab1a7eb2d28329e979431
msesno <msesno@gmail.com>
2019-05-02 17:50:32 -0400

    update friends image links

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,27 +1,27 @@
 var friends = [
     {
     "name": "Amir",
-    "photo": "#",
+    "photo": "/images/amir.jpg",
     "scores": ["5", "1", "4", "4", "5", "1", "2", "5", "4", "1"]
   }, {
-    "name": "Billy",
-    "photo": "#",
+    "name": "Bill",
+    "photo": "/images/billgates.jpg",
     "scores": ["4", "2", "5", "1", "3", "2", "2", "1", "3", "2"]
   }, {
     "name": "Jenny",
-    "photo": "#",
+    "photo": "/images/jenny.jpg",
     "scores": ["5", "2", "2", "2", "4", "1", "3", "2", "5", "5"]
   }, {
     "name": "Felicia",
-    "photo": "#",
+    "photo": "/images/felicia.jpg",
     "scores": ["3", "3", "4", "2", "2", "1", "3", "2", "2", "3"]
   }, {
     "name": "Donald",
-    "photo": "#",
+    "photo": "/images/donald.jpg",
     "scores": ["4", "3", "4", "1", "5", "2", "5", "3", "1", "4"]
   }, {
     "name": "Simmons",
-    "photo": "#",
+    "photo": "/images/simmons.jpg",
     "scores": ["4", "4", "2", "3", "2", "2", "3", "2", "4", "5"]
   }];
   
